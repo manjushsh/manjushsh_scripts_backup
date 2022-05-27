112594e5238bcf3529139e5563695ecff2ab27f5
Manjush Shetty <manjush@workplacefabric.com>
2022-05-27 19:32:25 +0530

    script updates

--- mount_and_dump.sh
+++ mount_and_dump.sh
@@ -4,7 +4,7 @@
 start_wireshark_dump() {
   # sudo dumpcap -D - for avaliable interfaces
   # https://www.wireshark.org/docs/wsug_html_chunked/AppToolsdumpcap.html
-  sudo dumpcap -i 2 -w "$1/$2/ms_netcap.pcapng" -b filesize:30000 -b files:100
+  sudo dumpcap -i 2 -w "$1/$2/$3" -b filesize:30000 -b files:100
   # Ring buffer for files of 100 30mb files
 }
 
@@ -24,11 +24,11 @@ init() {
   if montpoint -q $DIRECORY_TO_MOUNT; then
     echo "Directory is already mounted!"
     create_directory_and_files $MOUNT_TO $DUMP_DIRECTORY $FILE_NAME
-    start_wireshark_dump $MOUNT_TO $DUMP_DIRECTORY
+    start_wireshark_dump $MOUNT_TO $DUMP_DIRECTORY $FILE_NAME
   else
     sudo mount -t ntfs $DIRECORY_TO_MOUNT $MOUNT_TO
     create_directory_and_files $MOUNT_TO $DUMP_DIRECTORY $FILE_NAME
-    start_wireshark_dump $MOUNT_TO $DUMP_DIRECTORY
+    start_wireshark_dump $MOUNT_TO $DUMP_DIRECTORY $FILE_NAME
   fi
 }
 
