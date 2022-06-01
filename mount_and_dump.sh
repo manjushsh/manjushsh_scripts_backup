c2cb68e7ccc1270b3505fd1bc45e98b0524c19b3
Manjush Shetty <manjush@workplacefabric.com>
2022-06-01 14:33:15 +0530

    Interface entry by user

--- mount_and_dump.sh
+++ mount_and_dump.sh
@@ -2,10 +2,12 @@
 ### sudo fdisk -l
 
 start_wireshark_dump() {
-  # sudo dumpcap -D - for avaliable interfaces
   # https://www.wireshark.org/docs/wsug_html_chunked/AppToolsdumpcap.html
-  sudo dumpcap -i 2 -w "$1/$2/$3" -b filesize:30000 -b files:100
-  # Ring buffer for 100 files of 30mb each
+  interfaces=$(sudo dumpcap -D)   # sudo dumpcap -D - for avaliable interfaces
+  echo "Avilable interfaces: $interfaces"
+  echo "Enter your choice: "
+  read interface_choice
+  sudo dumpcap -i $interface_choice -w "$1/$2/$3" -b filesize:30000 -b files:100    # Ring buffer for 100 files of 30mb each
 }
 
 create_directory_and_files() {
