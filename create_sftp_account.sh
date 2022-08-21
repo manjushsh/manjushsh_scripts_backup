4242265846e7e8b89a9eb22ef08dcb731564ffbe
manjushsh <manjushsh@protonmail.com>
2022-08-21 11:34:04 +0530

    Move to dynamic group

--- create_sftp_account.sh
+++ create_sftp_account.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
+SFTP_USER_GROUP="sftp"
+SSH_CONFIG="# Config for SFTP Server
+Match group $SFTP_USER_GROUP
+ChrootDirectory /home
+X11Forwarding no
+AllowTcpForwarding no
+ForceCommand internal-sftp"
+
 install_dependenies() {
     sudo apt install ssh
 }
 
 config_ssh() {
-    config = '# Config for SFTP Server
-Match group sftp
-ChrootDirectory /home
-X11Forwarding no
-AllowTcpForwarding no
-ForceCommand internal-sftp'
-    if grep -Fxq "$config" /etc/ssh/sshd_config; then
+    if grep -Fxq "$SSH_CONFIG" /etc/ssh/sshd_config; then
         echo "Config is already added."
     else
-        echo config | sudo tee -a /etc/ssh/sshd_config
+        echo "$SSH_CONFIG" | sudo tee -a /etc/ssh/sshd_config
     fi
     sudo systemctl restart ssh
     create_sftp_user
 }
 
 create_sftp_user() {
-    sudo addgroup sftp
-    echo "Enter Username you want for SFTP user: "
+    sudo addgroup $SFTP_USER_GROUP
+    echo "Enter Username you want for SFTP user (Without space and special characters): "
     read username
-    sudo useradd -m $username -g sftp
+    sudo useradd -m $username -g $SFTP_USER_GROUP
     echo "Set Password for $username: "
     sudo passwd "$username"
     sudo chmod 700 /home/$username/
